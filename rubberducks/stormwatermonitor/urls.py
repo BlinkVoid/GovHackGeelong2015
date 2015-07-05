6427e293f99a9fc77e021620a14e7bed1dd823ee
Roger Zhou <nuoya.r.zhou@gmail.com>
2015-07-05 17:23:33 +1000

    fix a navigation issue from main to track page

--- rubberducks/stormwatermonitor/urls.py
+++ rubberducks/stormwatermonitor/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 	url(r'^$', views.main, name = 'main'),
 	#ex: /stormwatermonitor/main
 	url(r'^main$', views.main, name='main'),
-	url('/', views.main, name='main'),
+	#url('/', views.main, name='main'),
 	url(r'^track$', views.track, name='track'),
 ]
 
